8041a415f33ac8024e1cf8995dce4674d68830b9
Bienvenido <bienvenidoborico@gmail.com>
2025-08-08 16:59:47 +0200

    error prisma vercel

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ce67d11f85ae5549"; }
+/******/ 		__webpack_require__.h = function() { return "77554d336aa92892"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
